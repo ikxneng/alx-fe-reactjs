9367fe6c20bd81fce94a3a9f27c7628ca58297de
ikxneng <97749494+ikxneng@users.noreply.github.com>
2024-08-09 14:30:30 +0200

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,10 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import WelcomeMessage from './components/WelcomeMessage'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  // 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 
   return (
     <>
       <div>
@@ -28,6 +30,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      <WelcomeMessage />
     </>
   )
 }
