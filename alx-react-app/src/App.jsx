2ad159255a13c550793f8545d1b0d579144d00da
ikxneng <97749494+ikxneng@users.noreply.github.com>
2024-08-09 15:11:45 +0200

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,6 +6,7 @@ import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
+import UserProfile from './components/UserProfile'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -37,8 +38,8 @@ function App() {
       <WelcomeMessage />
       <Header />
       <MainContent />
+      <UserProfile name="Ikaneng" age="23" bio="Loves Golf and Photography" />
       <Footer />
-      
     </>
   )
 }
