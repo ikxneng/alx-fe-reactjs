56a117e1445675e362a8d916ce38f8aa9b008cbd
ikxneng <97749494+ikxneng@users.noreply.github.com>
2024-08-12 18:22:05 +0200

    Re Upload Initial Code Files

---
+++ alx-react-app-props/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer(){
+    return(
+        <footer style={{backgroundColor: 'grey', color: 'black', textAlign:'center'}}>
+            <p>&copy; 2023 City Lovers</p>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ alx-react-app-props/src/components/UserDetails.jsx
@@ -0,0 +1,13 @@
+import UserContext from './UserContext.jsx';
+
+function UserDetails() {
+    const UserData = UserContext(UserContext);
+    return (
+      <div>
+        <p>Name: {userData.name}</p>
+        <p>Email: {userData.email}</p>
+      </div>
+    );
+  }
+  
+  export default UserDetails;
