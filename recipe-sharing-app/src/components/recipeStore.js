af7877c2db71e7fe78d321bf9e06b89285f8f207
ikxneng <97749494+ikxneng@users.noreply.github.com>
2024-08-23 13:17:04 +0200

    Update recipeStore.js

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,12 +1,66 @@
-import {create} from 'zustand'
+import { create } from 'zustand';
 
 const useRecipeStore = create(set => ({
   recipes: [],
-  addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
-  setRecipes: (recipes) => set({ recipes }),
-  deleteRecipe: (id) => set((state) => ({recipes: state.recipes.filter(newRecipe => newRecipe.id !== id), })),
-  updateRecipe: (updatedRecipe) => set((state) => ({recipes: state.recipes.map(newRecipe => newRecipe.id === updatedRecipe.id ? updatedRecipe : newRecipe), })),
+  searchTerm: '',
+  filteredRecipes: [],
 
+
+  addRecipe: (newRecipe) => set(state => {
+    const updatedRecipes = [...state.recipes, newRecipe];
+    return { recipes: updatedRecipes, filteredRecipes: updatedRecipes.filter(recipe =>
+      recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase())
+    )};
+  }),
+
+
+  setRecipes: (recipes) => set(state => ({
+    recipes,
+    filteredRecipes: recipes.filter(recipe =>
+      recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase())
+    )
+  })),
+
+
+  deleteRecipe: (id) => set(state => {
+    const updatedRecipes = state.recipes.filter(recipe => recipe.id !== id);
+    return {
+      recipes: updatedRecipes,
+      filteredRecipes: updatedRecipes.filter(recipe =>
+        recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase())
+      )
+    };
+  }),
+
+
+  updateRecipe: (updatedRecipe) => set(state => {
+    const updatedRecipes = state.recipes.map(recipe =>
+      recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+    );
+    return {
+      recipes: updatedRecipes,
+      filteredRecipes: updatedRecipes.filter(recipe =>
+        recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase())
+      )
+    };
+  }),
+
+
+  setSearchTerm: (term) => set(state => {
+    const updatedSearchTerm = term;
+    return {
+      searchTerm: updatedSearchTerm,
+      filteredRecipes: state.recipes.filter(recipe =>
+        recipe.title.toLowerCase().includes(updatedSearchTerm.toLowerCase())
+      )
+    };
+  }),
+
+  filterRecipes: () => set(state => ({
+    filteredRecipes: state.recipes.filter(recipe =>
+      recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase())
+    )
+  })),
 }));
 
 export default useRecipeStore;
