94ff5d59802458830eb60857ad42af6e5bd35645
ikxneng <97749494+ikxneng@users.noreply.github.com>
2024-08-28 13:13:18 +0200

    Update RegistrationForm.jsx

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -2,43 +2,46 @@ import React from 'react';
 import { useState } from 'react';
 
 const RegistrationForm = () =>{
-    const [formData, setFormData] = useState({
-        username: '',
-        email: '',
-        password: '',
-    });
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-    const handleChange = (e) => {
-        const {name, value} = e.target;
-        setFormData({
-            ...formData,
-            [name]: value,
-        });
-    };
+    const handleUsernameChange = (e) => {
+        setUsername(e.target.value);
+    }
+
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value);
+    }
+
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value)
+    }
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log('Form Data Submitted', formData);
-    
-    };
+        const formData = {username, email, password };
+        console.log('Form Data Submitted: ', formData)
+    }
+
 
     return(
         <form onSubmit={handleSubmit}>
             <div>
             <label htmlFor="username">Username:</label>
-            <input type="text" id='username' name='username' value={formData.username} onChange={handleChange} required/>
+            <input type="text" id='username' name='username' value={username} onChange={handleUsernameChange} required/>
 
             </div>
 
             <div>
             <label htmlFor="email">Email:</label>
-            <input type="email" name="email" id="email" value={formData.email} onChange={handleChange} required />
+            <input type="email" name="email" id="email" value={email} onChange={handleEmailChange} required />
 
             </div>
 
             <div>
                 <label htmlFor="password">Password:</label>
-                <input type="password" id='password' name='password' value={formData.password} onChange={handleChange} required />
+                <input type="password" id='password' name='password' value={password} onChange={handlePasswordChange} required />
             </div>
             <button type="submit">Register</button>
 
