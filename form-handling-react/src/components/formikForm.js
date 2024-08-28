4114046d99055edc7ba1edbb44590620a6d0644c
ikxneng <97749494+ikxneng@users.noreply.github.com>
2024-08-28 13:31:06 +0200

    Update formikForm.js

--- form-handling-react/src/components/formikForm.js
+++ form-handling-react/src/components/formikForm.js
@@ -4,15 +4,15 @@ import * as Yup from 'yup';
 
 const FormikForm = () => {
     const validationSchema = Yup.object({
-        username: Yup.string()
-            .min(3, 'Username must be at least 3 characters')
-            .required('Username is required'),
-        email: Yup.string()
-            .email('Invalid email address')
-            .required('Email is required'),
-        password: Yup.string()
-            .min(6, 'Password must be at least 6 characters')
-            .required('Password is required'),
+        username: Yup.string().required('Username is required')
+            .min(3, 'Username must be at least 3 characters'),
+            
+        email: Yup.string().required('Email is required')
+            .email('Invalid email address'),
+           
+        password: Yup.string().required('Password is required')
+            .min(6, 'Password must be at least 6 characters'),
+            
     });
 
     const formik = useFormik({
