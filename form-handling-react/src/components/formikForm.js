ddf968a2208f0624ce11bade38ee328869fa33a2
ikxneng <97749494+ikxneng@users.noreply.github.com>
2024-08-28 13:29:17 +0200

    Update formikForm.js

--- form-handling-react/src/components/formikForm.js
+++ form-handling-react/src/components/formikForm.js
@@ -1,21 +1,19 @@
-import React from "react";
-import {useFormik} from 'formik';
+import React from 'react';
+import { useFormik, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 
-const formikForm = () => {
+const FormikForm = () => {
     const validationSchema = Yup.object({
         username: Yup.string()
-        .min(3, 'Username must be at least 3 Characters')
-        .required('Username is required'),
-
+            .min(3, 'Username must be at least 3 characters')
+            .required('Username is required'),
         email: Yup.string()
-        .email('Invalid Email address')
-        .required('Email is required'),
-
+            .email('Invalid email address')
+            .required('Email is required'),
         password: Yup.string()
-        .min(6, 'Password must be at least 6 characters')
-        .required('Password is required')
-    })
+            .min(6, 'Password must be at least 6 characters')
+            .required('Password is required'),
+    });
 
     const formik = useFormik({
         initialValues: {
@@ -25,67 +23,45 @@ const formikForm = () => {
         },
         validationSchema: validationSchema,
         onSubmit: (values) => {
-            console.log('Form data submitted: ', values);
+            console.log('Form data submitted:', values);
         },
     });
 
-    return(
+    return (
         <form onSubmit={formik.handleSubmit}>
             <div>
-            <label htmlFor="username">Username:</label>
-            <input 
-                type="text" 
-                id="username"
-                name="username"
-                value={formik.values.username}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur} 
-            />
-
-            {formik.touched.username && formik.errors.username? (
-                <div className="error">{formik.errors.username}</div>
-            ): null }
-
+                <label htmlFor="username">Username:</label>
+                <Field
+                    type="text"
+                    id="username"
+                    name="username"
+                />
+                <ErrorMessage name="username" component="div" className="error" />
             </div>
 
             <div>
-            <label htmlFor="email">Email:</label>
-            <input 
-                type="email" 
-                id="email"
-                name="email"
-                value={formik.values.email}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur} 
-            />
-
-            {formik.touched.email && formik.errors.email? (
-                <div className="error">{formik.errors.email}</div>
-            ): null }
-
+                <label htmlFor="email">Email:</label>
+                <Field
+                    type="email"
+                    id="email"
+                    name="email"
+                />
+                <ErrorMessage name="email" component="div" className="error" />
             </div>
 
             <div>
-            <label htmlFor="password">Password:</label>
-            <input 
-                type="password" 
-                id="password"
-                name="password"
-                value={formik.values.password}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur} 
-            />
-
-            {formik.touched.password && formik.errors.password? (
-                <div className="error">{formik.errors.password}</div>
-            ): null }
-
+                <label htmlFor="password">Password:</label>
+                <Field
+                    type="password"
+                    id="password"
+                    name="password"
+                />
+                <ErrorMessage name="password" component="div" className="error" />
             </div>
 
             <button type="submit">Register</button>
-
         </form>
     );
 };
 
-export default formikForm;
+export default FormikForm;
