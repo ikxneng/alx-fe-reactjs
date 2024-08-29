7fde405e74894108e695c27994552c2464d0f830
ikxneng <97749494+ikxneng@users.noreply.github.com>
2024-08-29 13:33:13 +0200

    Update Profile.jsx

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Outlet, Link, useParams } from "react-router-dom";
-
+import { Outlet, Link, useParams, Routes, Route } from "react-router-dom";
+import ProfileDetails from "./ProfileDetails";
+import ProfileSettings from './ProfileSettings'
 const Profile = () => {
     const { userId } = useParams();
     const user = {id: userId, name: `User ${userId}`, bio: 'Sample bio for the user'}
@@ -12,8 +13,10 @@ const Profile = () => {
             <p>Bio: {user.bio}</p> 
             <nav>
                 <ul>
-                    <li><Link to='details'>Profile Details</Link></li>
-                    <li><Link to='settings'>Profile Settings</Link></li>
+                    <Routes>
+                    <li><Route path='/details' element={<ProfileDetails />} />Profile Details </li>
+                    <li><Route to='settings' element={<ProfileSettings />}/> Profile Settings</li>
+                    </Routes>
                 </ul>
             </nav>
             <Outlet />
