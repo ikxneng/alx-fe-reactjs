f6330b1152f2f779b93e576caf5b4eaf582571f4
ikxneng <97749494+ikxneng@users.noreply.github.com>
2024-08-29 13:51:57 +0200

    Update Profile.jsx

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,23 +1,28 @@
+import React from "react";
+import { Outlet, Link, useParams, Routes, Route } from "react-router-dom";
+import ProfileDetails from "./ProfileDetails";
+import ProfileSettings from './ProfileSettings'
 
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import Login from "./components/Login";
-import Profile from "./components/Profile";
-import PrivateRoute from './components/PrivateRoute';
-import BlogPost from './components/BlogPost'; 
+const Profile = () => {
+    const { userId } = useParams();
+    const user = {id: userId, name: `User ${userId}`, bio: 'Sample bio for the user'}
 
-const App = () => (
-  <Router>
-      <div>
-        <Profile />
-          <Routes>
-              <Route path="/login" element={<Login />} />
-              <PrivateRoute path="/profile" element={<Profile />} />
-              <Route path="/blog/:id" element={<BlogPost />} /> 
-              <Route path="/" element={<h2>Home</h2>} />
-          </Routes>
-      </div>
-  </Router>
-);
+    return(
+        <div>
+            <h2>{user.name}'s Profile</h2>
+            <p>ID: {user.id}</p>
+            <p>Bio: {user.bio}</p> 
+            <nav>
+                <ul>
+                    <Routes>
+                    <li><Route path='/details' element={<ProfileDetails />} />Profile Details </li>
+                    <li><Route to='settings' element={<ProfileSettings />}/> Profile Settings</li>
+                    </Routes>
+                </ul>
+            </nav>
+            <Outlet />
+        </div>
+    )
+}
 
-export default App;
+export default Profile;
