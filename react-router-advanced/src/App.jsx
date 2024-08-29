5576eb287863c074132a6fd6ace75585660eec77
ikxneng <97749494+ikxneng@users.noreply.github.com>
2024-08-29 13:52:26 +0200

    Update App.jsx

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-d
 import Login from "./components/Login";
 import Profile from "./components/Profile";
 import PrivateRoute from './components/PrivateRoute';
+import BlogPost from './components/BlogPost'; 
 
 const App = () => (
   <Router>
@@ -12,6 +13,7 @@ const App = () => (
           <Routes>
               <Route path="/login" element={<Login />} />
               <PrivateRoute path="/profile" element={<Profile />} />
+              <Route path="/blog/:id" element={<BlogPost />} /> 
               <Route path="/" element={<h2>Home</h2>} />
           </Routes>
       </div>
