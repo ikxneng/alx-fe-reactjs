7e8d4b49b31cb6cdf6202559785141e76a7e4264
ikxneng <97749494+ikxneng@users.noreply.github.com>
2024-09-11 13:19:05 +0200

    Update HomePage.jsx

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -14,8 +14,8 @@ const HomePage = () => {
 
     return(
         <div className="container mx-auto p-4">
-            <h1 className="text-xl font-bold mb-6">Recipe List</h1>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            <h1 className="text-xl sm:text-xl font-bold mb-4 sm:mb-6">Recipe List</h1>
+            <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 sm:-6">
             {recipes.map((recipe) => (
                 <div
                     key={recipe.id}
@@ -25,10 +25,10 @@ const HomePage = () => {
                     <img 
                     src={recipe.image}
                     alt={recipe.title} 
-                    className="w-full h-48 object-cover" 
+                    className="w-full h-36 sm:h-48 object-cover"
                     />
-                    <div className="p-4">
-                    <h2 className="text-xl font-semibold mb-2">{recipe.title}</h2>
+                    <div className="p-3 sm:p-4">
+                    <h2 className="text-xl sm:text-xl font-semibold mb-1 sm:mb-2">{recipe.title}</h2>
                     <p className="text-gray-600 ">{recipe.summary}</p>
                     </div>
                 </div>
