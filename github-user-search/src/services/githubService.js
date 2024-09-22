0272e7afce29b398b33233eaea3653ba9bcbc70e
ikxneng <97749494+ikxneng@users.noreply.github.com>
2024-09-22 21:22:08 +0200

    Update githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const GITHUB_API_BASE_URL = 'https://api.github.com/search/users';
+const GITHUB_API_BASE_URL = 'https://api.github.com/search/users?q';
 
 export const fetchUserData = async (username, location = '', minRepos = '', page = 1) => {
   let query = `q=${username}`;
